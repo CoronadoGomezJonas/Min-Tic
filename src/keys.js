aadb9a6a8248261a5f95922b365ed5bf49756e17
CoronadoGomezJonas <coronadogomezjonas@gmail.com>
2021-10-15 23:41:17 -0500

    Se agrega el proyecto

---
+++ src/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'bzm5ssdf7qx4rfv2twig-mysql.services.clever-cloud.com',
+        user: 'bzm5ssdf7qx4rfv2twig',
+        password: 'doRxAWvdAi8lnHJIKSOn',
+        database: 'bzm5ssdf7qx4rfv2twig'
+    }
+
+};
+
